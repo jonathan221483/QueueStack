fe3ab20405329a8cb362ad0cf9602ce787c74ef3
jonathan221483 <captainberkut@gmail.com>
2023-05-01 19:10:22 +0600

    Queue add

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,5 +1,9 @@
 import java.util.NoSuchElementException;
 public class MyArrayListQueue {
-    private static MyArrayList stack = new MyArrayList();
+    private static MyArrayList queue = new MyArrayList();
     MyArrayListQueue(){}
+
+    public static void enqueue(Object o){
+        queue.add(o);
+    }
 }
