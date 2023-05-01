8562eedf08984cc91630d2e0a8ead19e8a06c258
jonathan221483 <captainberkut@gmail.com>
2023-05-01 20:07:17 +0600

    Stack comments

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,25 +1,25 @@
 import java.util.EmptyStackException;
 public class MyArrayListStack {
 
-    private static MyArrayList stack = new MyArrayList();
-    MyArrayListStack(){}
+    private static MyArrayList stack = new MyArrayList(); //create an instance of ArrayList
+    MyArrayListStack(){} //constructor for Stack
 
     public static void push(Object o){
         stack.add(o);
-    }
+        } //adds new element to stack
 
-    public static Object pop(){
-        Object element = stack.get(stack.size()-1);
-        stack.removeByIndex(stack.size()-1);
+    public static Object pop(){ //returns and deletes the front element of the stack
+        Object element = stack.get(stack.size()-1); //create a buffer variable that will store last element
+        stack.removeByIndex(stack.size()-1); //delete that element
         return element;
     }
     public static Object peek(){
         return stack.get(stack.size()-1);
-    }
+    } //returns front element of stack
 
-    public static boolean isEmpty(){
+    public static boolean isEmpty(){ //returns true if stack is empty
         for(int i = 0; i< stack.size(); i++){
-            if(stack.get(i)!=null)
+            if(stack.get(i)!=null) //check every element in the stack for null value
                 return false;
         }
         return true;
@@ -27,6 +27,6 @@ public class MyArrayListStack {
 
     public static int size(){
         return stack.size();
-    }
+    } //returns number of elements in the stack
 
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,29 +1,29 @@
 public class MyLinkedListStack {
-    static MyLinkedList stack = new MyLinkedList();
-    MyLinkedListStack(){}
+    static MyLinkedList stack = new MyLinkedList(); //create an instance of LinkedList
+    MyLinkedListStack(){} //constructor for Stack
 
-    public static void push(Object o){
+    public static void push(Object o){  //adds new element to stack
         stack.add(o);
     }
 
-    public static Object pop(){
-        Object element = stack.get(stack.size()-1);
-        stack.removeByIndex(stack.size()-1);
+    public static Object pop(){ //returns and deletes the front element of the stack
+        Object element = stack.get(stack.size()-1); //create a buffer variable that will store last element
+        stack.removeByIndex(stack.size()-1); //delete that element
         return element;
     }
     public static Object peek(){
         return stack.get(stack.size()-1);
-    }
+    } //returns front element of stack
 
-    public static boolean isEmpty(){
+    public static boolean isEmpty(){ //returns true if stack is empty
         for(int i = 0; i< stack.size(); i++){
-            if(stack.get(i)!=null)
+            if(stack.get(i)!=null) //check every element in the stack for null value
                 return false;
         }
         return true;
     }
     public static int size(){
         return stack.size();
-    }
+    } //returns number of elements in the stack
 
 }
