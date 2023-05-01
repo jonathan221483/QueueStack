98371ce15093d8e40f770c9facfa31461335f9f9
jonathan221483 <captainberkut@gmail.com>
2023-05-01 18:43:22 +0600

    Stack done

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
         //////////////////////
         MyArrayListStack a = new MyArrayListStack();
         a.push(3);
-        //a.push(6);
+        a.push(6);
         //a.push(9);
         System.out.println(a.size());
         System.out.println(a.pop());
-        System.out.println(a.isEmpty());
+        System.out.println(a.peek());
         System.out.println(a.size());
 
         //////////////////////
