cb1d17cc74612ab6297273af97c8e114abbfc4c0
jonathan221483 <captainberkut@gmail.com>
2023-05-01 19:55:20 +0600

    (Linked list) Stack push, pop

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,14 @@
 public class MyLinkedListStack {
+    static MyLinkedList stack = new MyLinkedList();
+    MyLinkedListStack(){}
+
+    public static void push(Object o){
+        stack.add(o,0);
+    }
+
+    public static Object pop(){
+        Object element = stack.get(stack.size()-1);
+        stack.removeByIndex(stack.size()-1);
+        return element;
+    }
 }
